3e3adbdd74d98fffde900bb6a39621f19090641f
Marcin Orzełowski <marcin.hubert.orzelowski@gmail.com>
2020-05-24 10:39:05 +0200

    first commit

---
+++ src/main/java/com/orzelowski/laboratory3/Laboratory3Application.java
@@ -0,0 +1,13 @@
+package com.orzelowski.laboratory3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratory3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboratory3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/orzelowski/laboratory3/Laboratory3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orzelowski.laboratory3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratory3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
